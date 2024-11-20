bece07254c82e908e9de8e3ca02fcc3f0552676a
Brad <bradley.warman@gmail.com>
2024-11-20 14:03:27 +1030

    Countdown timer

---
+++ assets/js/countdown.js
@@ -0,0 +1,21 @@
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+
+    var eventDate = "2025-01-07"; //Use the YYYY-MM-DD format
+    
+    const countDown = new Date(eventDate).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+          //seconds
+        }, 0)
+    }());
